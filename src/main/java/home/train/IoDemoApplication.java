3632eb09d94d3feba90db26a59f871f6b5eb7f3b
hosseinnicksecond <hosseinnickseresht1986@gmail.com>
2021-11-04 16:51:02 +0330

    first commit

---
+++ src/main/java/home/train/IoDemoApplication.java
@@ -0,0 +1,13 @@
+package home.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/train/IoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package home.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
